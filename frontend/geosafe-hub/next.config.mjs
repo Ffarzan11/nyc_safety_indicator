3bb61474d325716c97ae2d871e1ecf2fb13b3cd2
farhikhta farzan <farhikhtafarzan@gmail.com>
2025-05-06 18:34:20 -0400

    config updated

--- frontend/geosafe-hub/next.config.mjs
+++ frontend/geosafe-hub/next.config.mjs
@@ -24,15 +24,7 @@ const nextConfig = {
 }
 
 mergeConfig(nextConfig, userConfig)
-nextConfig.headers = async () => [
-  {
-    source: "/(.*)",
-    headers: [
-      { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-      { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-    ],
-  },
-];
+
 
 function mergeConfig(nextConfig, userConfig) {
   if (!userConfig) {
